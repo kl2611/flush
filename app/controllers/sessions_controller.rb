98c6841812c01295c8ed87e5a9538f2fff9567be
Kelly <kelly.liu@columbia.edu>
2016-01-13 16:31:35 -0800

    login/logout now works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 
         if user.nil?
             flash.now[:errors] = ["Incorrect username and/or password"]
-            redirect_to new_session_url
+            render :new
         else
             login_user!(user)
             redirect_to :root
